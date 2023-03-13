9230950d81c4ac11452792e4617d69dda7cf8b7e
Billy <bimganda@gmail.com>
2023-03-13 05:18:20 -0400

    added usersController in presentation

---
+++ Ecommerce.Presentation/Controllers/usersController.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Application.Repositories;
+using Ecommerce.Application.Services;
+using Ecommerce.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public usersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<IList<User>> GetAllUsers()
+        {
+            var users = _userService.GetAllUsers();
+            return Ok(users);
+        }
+    }
+}
