69e37368f47c71add4ed266533e4b4d662b589d4
Billy <bimganda@gmail.com>
2023-03-13 08:09:02 -0400

    created Brand class in Domain

---
+++ Ecommerce.Domain/Entities/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Domain.Entities
+{
+    public class Brand
+    {
+        public int brand_id { get; set; }
+        public string brand_name { get; set; } = string.Empty;
+    }
+}
