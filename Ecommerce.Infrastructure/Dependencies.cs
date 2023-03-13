59d44399fd71f26ff15d5a5b54958360fde2705c
Billy <bimganda@gmail.com>
2023-03-13 06:20:20 -0400

    added Dependencies class in Infrastructure

---
+++ Ecommerce.Infrastructure/Dependencies.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ecommerce.Infrastructure
+{
+    public static class Dependencies
+    {
+        public static void ConfigureServices(IConfiguration configuration, IServiceCollection services)
+        {
+            services.AddDbContext<>(c => c.UseSqlServer(configuration.GetConnectionString("MSSQLServerConnection")));
+        }
+    }
+}
