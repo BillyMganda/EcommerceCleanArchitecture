94b7b77a96ed37a4da7e51b9b57208a3c34e8c09
Billy <bimganda@gmail.com>
2023-03-13 07:55:52 -0400

    added LoginRequest

---
+++ Ecommerce.Contracts/Authentication/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Contracts.Authentication
+{
+    public class LoginRequest
+    {
+        public string email { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
