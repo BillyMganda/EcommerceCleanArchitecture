cad791c3cf7346320552a8bd974c8de9052c3bd5
Billy <bimganda@gmail.com>
2023-03-13 05:05:48 -0400

    created UserRepository in infra to implement IUserRepository in application

---
+++ Ecommerce.Infrastructure/RepositoryImplementation/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ecommerce.Application.Repositories;
+using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Infrastructure.RepositoryImplementation
+{
+    public class UserRepository : IUserRepository
+    {
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
