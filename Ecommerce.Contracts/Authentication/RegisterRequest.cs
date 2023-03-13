303e09b6817885e8f0edcb7c58c12f4f3b73e4a7
Billy <bimganda@gmail.com>
2023-03-13 07:54:33 -0400

    added RegisterRequest

---
+++ Ecommerce.Contracts/Authentication/RegisterRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Contracts.Authentication
+{
+    public class RegisterRequest
+    {
+        public string email { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+    }
+}
