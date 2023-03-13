d2ad766ccacd465df5d061266744e76af64b3c52
Billy <bimganda@gmail.com>
2023-03-13 08:26:47 -0400

    implemented IBrandsService using IbrandsRepository in constructor

--- Ecommerce.Application/Brand/BrandsService.cs
+++ Ecommerce.Application/Brand/BrandsService.cs
@@ -1,6 +1,32 @@
-﻿namespace Ecommerce.Application.Brand
+﻿using Ecommerce.Domain.Entities;
+
+namespace Ecommerce.Application.Brand
 {
-    public class BrandsService
+    public class BrandsService : IBrandsService
     {
+        private readonly IbrandsRepository _brandRepository;
+        public BrandsService(IbrandsRepository brandRepository)
+        {
+            _brandRepository = brandRepository;
+        }
+        Task<List<Brands>> IBrandsService.GetAllBrands()
+        {
+            return _brandRepository.GetAllBrands();
+        }
+
+        Task<Brands> IBrandsService.GetBrandById(int id)
+        {
+            return _brandRepository.GetBrandById(id);
+        }
+
+        Task<Brands> IBrandsService.UpdateBrand(Brands brand)
+        {
+            return _brandRepository.UpdateBrand(brand);
+        }
+
+        Task<Brands> IBrandsService.DeleteBrand(int id)
+        {
+            return _brandRepository.DeleteBrand(id);
+        }
     }
 }
