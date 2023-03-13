86795409a8325491b0e37c24412be23206d74e11
Billy <bimganda@gmail.com>
2023-03-13 09:18:04 -0400

    created BrandsController

---
+++ Ecommerce.Presentation/Controllers/BrandsController.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Application.Brand;
+using Ecommerce.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Presentation.Controllers
+{
+    [Route("brands")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandsService _brandsService;
+        public BrandsController(IBrandsService brandsService)
+        {
+            _brandsService = brandsService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Brands>> GetBrands()
+        {
+            var brands = _brandsService.GetAllBrands();
+            return Ok(brands);
+        }
+    }
+}
