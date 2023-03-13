efb8c9c1fd99ff04aeedf11171917bd42c2f4435
Billy <bimganda@gmail.com>
2023-03-13 08:27:57 -0400

    updated Brands class

--- Ecommerce.Domain/Entities/Brands.cs
+++ Ecommerce.Domain/Entities/Brands.cs
@@ -1,8 +1,12 @@
-﻿namespace Ecommerce.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Domain.Entities
 {
     public class Brands
     {
+        [Key]
         public int brand_id { get; set; }
+        [Required]
         public string brand_name { get; set; } = string.Empty;
     }
 }
