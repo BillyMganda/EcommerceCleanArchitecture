0ab7481af888758c1dab6419e165ec9118e95f0b
Billy <bimganda@gmail.com>
2023-03-20 10:05:29 -0400

    updated swaggergen to include swaggerdoc

--- Ecommerce.Presentation/Program.cs
+++ Ecommerce.Presentation/Program.cs
@@ -3,17 +3,22 @@ using Ecommerce.Application.Brand;
 using Ecommerce.Infrastructure;
 using Ecommerce.Infrastructure.Brand;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 //MINE
 builder.Services.AddDbContext<EcommerceDataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MSSQLServerConnection")));
 builder.Services.AddApplication(); // DI in application layer
 builder.Services.AddInfrastructure(); // DI in infrastructure layer
+builder.Services.AddSwaggerGen(c => 
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ecommerce.API", Version = "v1" });
+});
 
 
 var app = builder.Build();
